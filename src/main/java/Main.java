c90559f71ad225ccc1fe28ef9cc9a9c943e55888
BowWarrior <ajf4500@gmail.com>
2025-12-16 22:31:44 -0600

    added useful comments about running the app

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,16 +7,18 @@
 /*
 to push to github in terminal:
 
-
 1. cd into the folder
 2. run 'git add .'
 3. run 'git commit -m "message to github here"'
 4. run 'git push origin changes'
 5. check I'm on the '*main' branch using 'git branch'. If not, run 'git checkout main'
 6. git pull origin 'changes'
+*/
 
+//to run the java file in terminal (from directory RevTacToe folder):
+//DOES NOT WORK WITH INTELLIJ. ONLY VS CODE TERMINAL, POWERSHELL, AND CMD
+//java -cp target\classes Main
 
- */
 public class Main {
     public static void main(String[] args) {
         new myFrame();
